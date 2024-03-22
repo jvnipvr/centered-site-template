8de87c6ab881e34de711454e14585bb82cc9da70
jvnipvr <160485621+jvnipvr@users.noreply.github.com>
2024-03-21 23:43:21 -0700

    Created files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='en-US'>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Site Title</title>
+	<link href='/csspage.css' rel='stylesheet'>
+</head>
+<body>
+	<div class='center' style='text-align:center;'>
+	<p>
+		Disclaimer content
+		<br>
+		<br>
+		<a href='main.php'>Agreement URL</a>
+	</p>
+	</div>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='en-US'>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Site Title</title>
+	<link href='/csspage.css' rel='stylesheet'>
+</head>
+<body>
+	<div class='center' style='text-align:center;'>
+	<p>
+		Site content
+	</p>
+	</div>
+</body>
+</html>
